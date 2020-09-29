ac123df66333ce72e7300458438f55c83f063af2
santiagoLoM <slm181291@gmail.com>
2020-09-28 21:14:26 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Daniel.
+          Edit <code>src/App.js</code> D&S.
         </p>
         <a
           className="App-link"
